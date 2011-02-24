195c6e4e48838b2435be059327a93b36efd2403b
Rob Speer <rspeer@mit.edu>
2011-02-24 03:10:55 -0500

    amusing sufjan stevens post

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-PYTHONPATH=`pwd` hyde gen && rsync -avz deploy/ rspeer@ml.media.mit.edu:public_html/ && rsync -avz media rspeer@ml.media.mit.edu:public_html/
+PYTHONPATH=`pwd` hyde gen && rsync -avz --size-only deploy/ rspeer@ml.media.mit.edu:public_html/ && rsync -avz media rspeer@ml.media.mit.edu:public_html/
